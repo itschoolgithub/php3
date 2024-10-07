8a0fda3bd85c1c9220ac4d9974d356a2b24ab910
itschool <itschool@tou.edu.kz>
2024-10-07 22:00:18 +0500

    init

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    $a = 2;
+    $b = 3;
+
+    // $c = $a + $b;
+    // echo $c;
+
+    // echo $a + $b;
+    // echo $a - $b;
+    // echo $a * $b;
+    // echo $a / $b;
+
+    // echo $a % $b;
+    // echo $a ** $b;
+
+    // $c = 4;
+    // echo "<p>{$c}</p>"; // 4
+    // echo "<p>";
+    // echo $c++; // 4
+    // echo ++$c; // 5
+    // echo "</p>";
+    // echo "<p>{$c}</p>"; // 5
+    // $c = $c + 2;
+    // $c += 2;
+    // echo $c;
+
+    // $c = $c + 1;
+    // $c += 1;
+    // $c++;
+    // $c--;
+    // ++$c;
+    // --$c;
+    
+    $a = "Hello";
+    $b = "World";
+
+    echo $a . $b;
+
+    $c = $a . $b . $a . $b . "!";
